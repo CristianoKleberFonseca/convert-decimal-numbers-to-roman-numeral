1f2541e895120f4450fa477bd12f94c28363f49f
Cristiano Fonseca <ckfonseca@gmail.com>
2020-04-13 23:46:57 -0300

    Ajuste na menssagem.

--- src/main/java/br/com/kata/exceptions/MessagesExceptions.java
+++ src/main/java/br/com/kata/exceptions/MessagesExceptions.java
@@ -7,5 +7,5 @@ public class MessagesExceptions {
 	public static final String VALUE_ZERO_IS_NOT_VALID = "The value 0(zero) is not valid.";
 	public static final String VALUE_EXCEEDED = "The value \"%d\" exceeded the limit.";
 	public static final String ROMAN_NUMERAL_DOES_NOT_INFORMED = "Roman numeral does not informed.";
-	public static final String ROMAN_NUMERAL_IS_NOT_VALID = "The roman numeral \"%s\" is not valid or exceeded the limit.";
+	public static final String ROMAN_NUMERAL_IS_NOT_VALID = "The roman numeral \"%s\" is not valid.";
 }
